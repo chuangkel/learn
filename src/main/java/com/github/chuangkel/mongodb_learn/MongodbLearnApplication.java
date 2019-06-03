d088664780a52a2dec53ba69bd4a0e0eff91346a
2010344124@qq.com <2010344124@qq.com>
2019-06-03 19:40:05 +0800

    learn

---
+++ src/main/java/com/github/chuangkel/mongodb_learn/MongodbLearnApplication.java
@@ -0,0 +1,13 @@
+package com.github.chuangkel.mongodb_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbLearnApplication.class, args);
+	}
+
+}
