488fadb2f8b3a544be2e5e591086fcefdd1dd186
2010344124@qq.com <2010344124@qq.com>
2019-06-11 19:43:47 +0800

    dindin-web

---
+++ dindin-web/src/router/index.js
@@ -0,0 +1,103 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import ArticleList from '@/components/ArticleList'
+import CateMana from '@/components/CateMana'
+import DataCharts from '@/components/DataCharts'
+import PostArticle from '@/components/PostArticle'
+import UserMana from '@/components/UserMana'
+import BlogDetail from '@/components/BlogDetail'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '登录',
+      hidden: true,
+      component: Login
+    }, {
+      path: '/home',
+      name: '',
+      component: Home,
+      hidden: true
+    }, {
+      path: '/home',
+      component: Home,
+      name: '活动管理',
+      iconCls: 'fa fa-file-text-o',
+      children: [
+        {
+          path: '/articleList',
+          name: '活动列表',
+          component: ArticleList,
+          meta: {
+            keepAlive: true
+          }
+        }, {
+          path: '/postArticle',
+          name: '发起活动',
+          component: PostArticle,
+          meta: {
+            keepAlive: false
+          }
+        }, {
+          path: '/blogDetail',
+          name: '活动详情',
+          component: BlogDetail,
+          hidden: true,
+          meta: {
+            keepAlive: false
+          }
+        }, {
+          path: '/editBlog',
+          name: '编辑活动',
+          component: PostArticle,
+          hidden: true,
+          meta: {
+            keepAlive: false
+          }
+        }
+      ]
+    }, {
+      path: '/home',
+      component: Home,
+      name: '奖励管理',
+      children: [
+        {
+          path: '/user',
+          iconCls: 'fa fa-user-o',
+          name: '奖励管理',
+          component: UserMana
+        }
+      ]
+    }, {
+      path: '/home',
+      component: Home,
+      name: '抽奖管理',
+      children: [
+        {
+          path: '/cateMana',
+          iconCls: 'fa fa-reorder',
+          name: '抽奖管理',
+          component: CateMana
+        }
+      ]
+    }, {
+      path: '/home',
+      component: Home,
+      name: '信息管理',
+      iconCls: 'fa fa-bar-chart',
+      children: [
+        {
+          path: '/charts',
+          iconCls: 'fa fa-bar-chart',
+          name: '信息管理',
+          component: DataCharts
+        }
+      ]
+    }
+  ]
+})
---
+++ dindin-web/src/utils/api.js
@@ -0,0 +1,91 @@
+import axios from 'axios'
+
+let base = 'http://127.0.0.1:80';
+
+export const postRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    transformRequest: [function (data) {
+      let ret = ''
+      for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }],
+    withCredentials: true,
+    headers: {
+      // 'Access-Control-Allow-Origin':'*'
+      // ,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      // "Access-Control-Expose-Headers":"Access-Control-*",
+      // "Access-Control-Allow-Headers": "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept",
+      // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+      // 'Allow': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
+    }
+  });
+}
+
+export const postRequestJson = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    withCredentials: true,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+}
+
+export const uploadFileRequest = (url, params) => {
+  return axios({
+    method: 'post',
+    url: `${base}${url}`,
+    data: params,
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+}
+export const putRequest = (url, params) => {
+  return axios({
+    method: 'put',
+    url: `${base}${url}`,
+    data: params,
+    transformRequest: [function (data) {
+      let ret = ''
+      for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }],
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+}
+export const deleteRequest = (url) => {
+  return axios({
+    method: 'delete',
+    url: `${base}${url}`
+  });
+}
+export const getRequest = (url,params) => {
+  return axios({
+    method: 'get',
+    data:params,
+    transformRequest: [function (data) {
+      let ret = ''
+      for (let it in data) {
+        ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+      }
+      return ret
+    }],
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    url: `${base}${url}`
+  });
+}
